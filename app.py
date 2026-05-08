bb40c73b67dc2e72e97beeafe8a00643bd93bf87
Pedro Neto <pccn2003@gmail.com>
2026-05-08 17:28:31 +0200

    primeiro prototipo

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from datetime import datetime
+
+# CONFIGURAÇÃO DA PÁGINA
+st.set_page_config(
+    page_title="Nutri Check",
+    page_icon="🥗",
+    layout="centered"
+)
+
+# TÍTULO
+st.title("🥗 Check-in Intestinal")
+
+st.write("Responda rapidamente as perguntas do dia.")
+
+st.divider()
+
+# PERGUNTA 1
+banheiro = st.radio(
+    "Quantas vezes você foi ao banheiro hoje?",
+    ["0", "1", "2", "3", "4 ou mais"]
+)
+
+st.divider()
+
+# PERGUNTA 2
+boiavam = st.radio(
+    "As fezes boiavam?",
+    ["Sim", "Não", "Não me lembro"]
+)
+
+st.divider()
+
+# BOTÃO
+if st.button("Salvar respostas"):
+
+    dados = {
+        "data": str(datetime.now()),
+        "banheiro": banheiro,
+        "boiavam": boiavam
+    }
+
+    st.success("✅ Respostas salvas com sucesso!")
+
+    st.write("Resumo do dia:")
+    st.json(dados)
